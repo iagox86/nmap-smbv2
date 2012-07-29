c00f6d1224cdb1f73b791ab2f8ef34c02cbac6eb
Ron <ron@skullsecurity.net>
2012-07-29 14:51:09 -0500

    Adding a small testing framework that I'm going to work on improving

---
+++ nselib/test.lua
@@ -0,0 +1,69 @@
+---
+-- TODO
+--
+--
+
+local nmap = require "nmap"
+local stdnse = require "stdnse"
+local string = require "string"
+
+_ENV = stdnse.module("test", stdnse.seeall)
+
+Test =
+{
+  new = function(self)
+    local o = { }
+    setmetatable(o, self)
+    self.__index = self
+
+    o.success = 0
+    o.fail = 0
+
+    return o
+  end,
+
+  display = function(self, header, value, options)
+    io.write(header .. "\n")
+    if(options.binary) then
+      io.write("\n")
+      nsedebug.print_hex(value)
+    elseif(options.hex) then
+      io.write(string.format("%x", value) .. "\n")
+    elseif(options.format_string) then
+      io.write(string.format(format_string, value) .. "\n")
+    else
+      io.write(value .. "\n")
+    end
+  end,
+
+  call = function(self, name, value, expected, options)
+    options = options or {}
+
+    if(value ~= expected) then
+      self.fail = self.fail + 1
+
+      io.write("FAIL: " .. name .. ": ")
+      self:display("Expected: ", expected, options)
+      self:display("Found:    ", found, options)
+    else
+      self.success = self.success + 1
+      io.write("PASS: " .. name)
+      if(options.display_pass) then
+        self:display("Data:     ", expected, options)
+      else
+        io.write("\n")
+      end
+    end
+  end,
+
+  report = function(self)
+    print(("-"):rep(80))
+    print("SUCCESS  : " .. self.success)
+    print("FAIL     : " .. self.fail)
+    print("--------")
+    print("RESULT   : " .. (self.success / (self.success + self.fail)))
+    print(("-"):rep(80))
+  end
+}
+
+return _ENV;
